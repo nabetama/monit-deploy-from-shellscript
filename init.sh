e1be81559f7197e4e1af2a60ebd1bc495d5cac63
nabetama <mao.nabeta@gmail.com>
2016-04-01 14:10:27 +0900

    [Add] init.sh
    ./etc/monit/conf.d/gunicorn置換用のshell

---
+++ init.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+if [ $# -gt 1 -o $# -eq 0 ]; then
+  echo "Usage: init.sh <PROCESS NAME>"
+  exit 1
+fi
+
+PROCESS_NAME=$1
+
+# BSD, GNUどちらでも動くはず
+sed -i '' -e "s/PROCESS_NAME/$PROCESS_NAME/" ./etc/monit/conf.d/gunicorn
+
+echo "/etc/monit/conf.d/gunicorn を置換したよ!"
+echo "-------------------------------------------------------------------------"
+cat ./etc/monit/conf.d/gunicorn
+echo "-------------------------------------------------------------------------"
